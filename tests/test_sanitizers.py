15d3e86bd0d84822fe3ea547b8c67cc522cfc69a
KS Chan <mrkschan@gmail.com>
2012-04-19 20:56:39 +0800

    Included test cases for tab_in_indent_sanitizer()

--- tests/test_sanitizers.py
+++ tests/test_sanitizers.py
@@ -157,3 +157,66 @@ def test_indent_with_non_tab_sanitizer():
     now = gfws.indent_with_non_tab_sanitizer(before)
     assert now == after, \
            'before: "%s", after: "%s", now: "%s"' % (before, after, now)
+
+
+def test_tab_in_indent_sanitizer():
+    # NOTE: The defaut TAB_WIDTH is 8.
+    before = ''
+    after = ''
+    now = gfws.tab_in_indent_sanitizer(before)
+    assert now == after, \
+           'before: "%s", after: "%s", now: "%s"' % (before, after, now)
+
+    before = ' '
+    after = ' '
+    now = gfws.tab_in_indent_sanitizer(before)
+    assert now == after, \
+           'before: "%s", after: "%s", now: "%s"' % (before, after, now)
+
+    before = '  	'
+    after = '  	'
+    now = gfws.tab_in_indent_sanitizer(before)
+    assert now == after, \
+           'before: "%s", after: "%s", now: "%s"' % (before, after, now)
+
+    before = 'abc'
+    after = 'abc'
+    now = gfws.tab_in_indent_sanitizer(before)
+    assert now == after, \
+           'before: "%s", after: "%s", now: "%s"' % (before, after, now)
+
+    before = ' abc'
+    after = ' abc'
+    now = gfws.tab_in_indent_sanitizer(before)
+    assert now == after, \
+           'before: "%s", after: "%s", now: "%s"' % (before, after, now)
+
+    before = '	abc'
+    after = '        abc'
+    now = gfws.tab_in_indent_sanitizer(before)
+    assert now == after, \
+           'before: "%s", after: "%s", now: "%s"' % (before, after, now)
+
+    before = ' 	abc'
+    after = '        abc'
+    now = gfws.tab_in_indent_sanitizer(before)
+    assert now == after, \
+           'before: "%s", after: "%s", now: "%s"' % (before, after, now)
+
+    before = '	        abc'
+    after = '                abc'
+    now = gfws.tab_in_indent_sanitizer(before)
+    assert now == after, \
+           'before: "%s", after: "%s", now: "%s"' % (before, after, now)
+
+    before = '	          	abc'
+    after = '                        abc'
+    now = gfws.tab_in_indent_sanitizer(before)
+    assert now == after, \
+           'before: "%s", after: "%s", now: "%s"' % (before, after, now)
+
+    before = 'abc	efg'
+    after = 'abc	efg'
+    now = gfws.tab_in_indent_sanitizer(before)
+    assert now == after, \
+           'before: "%s", after: "%s", now: "%s"' % (before, after, now)
