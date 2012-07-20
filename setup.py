24dd4776f50808527c18e2bfcf1c1142398cea06
KS Chan <mrkschan@gmail.com>
2012-07-20 09:53:04 +0800

    Packaging for setuptools

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+desc = 'Fix whitespace problem according to git-config'
+__version__ = ('0', '1', '0')
+
+cwd = os.path.abspath(os.path.dirname(__file__))
+
+
+def pip_requirements():
+    '''Return non-comment lines from requirements.txt'''
+    with open(os.path.join(cwd, 'requirements.txt'), 'r') as f:
+        requirements = [i for i in f.readlines() if i.strip()[0] != '#']
+    return requirements
+
+
+setup(
+    name='git-fix-whitespace',
+    version='.'.join(__version__),
+    description=desc,
+    author='mrkschan',
+    author_email='mrkschan@gmail.com',
+    packages=find_packages(),
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'git-fix-whitespace = git_fix_whitespace.git_fix_whitespace:main',
+        ]
+    },
+    install_requires=pip_requirements(),
+)
